1955f6361cf5629a49fa8714ce1ccf5fd4847223
Gastonx <56830127+Gastonx@users.noreply.github.com>
2020-09-02 11:57:59 +0200

    CheckPoint

---
+++ GameJamIIM/Assets/Scripts/Player/PlayerRespawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    #region variable
+    [SerializeField] Vector3 RespawnPoint;
+
+    #endregion
+
+    #region System
+    
+    void Start()
+    {
+        RespawnPoint = transform.position;
+    }
+
+    
+    void Update()
+    {
+        Respawn();
+    }
+    private void Respawn() 
+    {
+     transform.position = RespawnPoint;
+      
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "CheckPoint")
+        {
+            RespawnPoint = collision.transform.position;
+            Destroy(collision.gameObject.GetComponent<BoxCollider2D>());
+        }
+    }
+
+    #endregion
+}
