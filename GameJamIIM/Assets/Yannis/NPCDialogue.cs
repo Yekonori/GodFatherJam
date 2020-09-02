225c500ad5a7214ae2681e2e5d734a1d23975a72
Yekonori <yannisdemacedo@gmail.com>
2020-09-02 13:12:31 +0200

    BulleDialogue

---
+++ GameJamIIM/Assets/Yannis/NPCDialogue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDialogue : MonoBehaviour
+{
+    #region Script Parameters
+
+    [SerializeField] private Sprite dialogue;
+
+    #endregion
+}
