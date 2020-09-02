6bd14806bcd3eeadd7951f0b7f19237fc073d2e3
Gastonx <56830127+Gastonx@users.noreply.github.com>
2020-09-02 18:02:19 +0200

    Obstacle+Caméra

---
+++ GameJamIIM/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float MassCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.gameObject.GetComponent<Rigidbody2D>().mass > MassCheck)
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+             
+            }
+            else
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+            }
+        }
+    }
+}
