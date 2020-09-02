622053b2af31170b8bec35b532dee9c19fa43508
Yekonori <yannisdemacedo@gmail.com>
2020-09-02 14:22:52 +0200

    NPC Dialogue Sprite On/Off

--- GameJamIIM/Assets/Yannis/NPCDialogue.cs
+++ GameJamIIM/Assets/Yannis/NPCDialogue.cs
@@ -6,7 +6,36 @@ public class NPCDialogue : MonoBehaviour
 {
     #region Script Parameters
 
-    [SerializeField] private Sprite dialogue;
+    [SerializeField] private Sprite dialogueSprite;
+    [SerializeField] private GameObject dialogue;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        dialogue.SetActive(false);
+
+        SpriteRenderer spriteRenderer = dialogue.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = dialogueSprite;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            dialogue.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            dialogue.SetActive(false);
+        }
+    }
 
     #endregion
 }
