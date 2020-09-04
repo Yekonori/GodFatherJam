543786b40d2ebdaee6d08391da81c6bf4b1b59de
vngupro <60599812+vngupro@users.noreply.github.com>
2020-09-04 10:27:15 +0200

    bush animation + trigger en passant

---
+++ GameJamIIM/Assets/Scripts/BushTriggerAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushTriggerAnimation : MonoBehaviour
+{
+    Animator animBush;
+    private void Start()
+    {
+        animBush = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            animBush.SetBool("isBushing", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        animBush.SetBool("isBushing", false);
+    }
+}
