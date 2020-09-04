431a89ec7747836dd4cd634f26e587f7259f07b3
vngupro <60599812+vngupro@users.noreply.github.com>
2020-09-04 11:37:15 +0200

    anim plate, checkpoint, spike

---
+++ GameJamIIM/Assets/Scripts/TriggerAnim/PressurePlateTriggerAnim.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateTriggerAnim : MonoBehaviour
+{
+    Animator animPP;
+    [SerializeField] bool isLight = false;
+    [SerializeField] int counter = 1;
+    private void Start()
+    {
+        animPP = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (!isLight)
+            {
+                StartCoroutine(PPAnimation());
+            }
+            else
+            {
+                animPP.SetBool("isActivate", false);
+                animPP.SetBool("isOn", false);
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (isLight)
+            {
+                isLight = false;
+            }
+            else
+            {
+                isLight = true;
+            }
+        }
+    }
+
+    IEnumerator PPAnimation()
+    {
+        animPP.SetBool("isActivate", true);
+        yield return null;
+        animPP.SetBool("isOn", true);
+    }
+}
